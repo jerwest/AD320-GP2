6a54d9118ba625383749722efb392fb4084b0d06
Iryna <iryna.sherepot@gmail.com>
2019-02-19 12:45:08 -0800

    Solving img problem

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/login', function(req, res, next) {
 
-  res.render('index', { title: 'Home Page' });
+  res.render('login', { title: 'Express' });
 
 });
 
