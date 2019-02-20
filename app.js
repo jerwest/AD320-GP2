a1705e4323a8fba4d5d21dd3f3a46bd72bdfa3bf
Iryna <iryna.sherepot@gmail.com>
2019-02-19 17:13:16 -0800

    Working navigation

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var homeRouter = require('./routes/home');
+var activityRouter = require('./routes/activity');
+var contactRouter = require('./routes/contact');
+var logoutRouter = require('./routes/logout')
 
 var app = express();
 
@@ -20,7 +23,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/home', homeRouter);
+app.use('/activity', activityRouter);
+app.use('/contact', contactRouter);
+app.use('/logout', logoutRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('newuser', { title: 'Express' });
+  res.render('logout', { title: 'Log Out' });
 
 });
 
